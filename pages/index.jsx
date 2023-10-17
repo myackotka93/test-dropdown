26cb4f5c66376047ed0e30d853a106b59041d940
Valeriya Kuznetsova <88514633+myackotka93@users.noreply.github.com>
2023-10-17 16:08:25 +0200

    add getStaticProps

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,14 +3,31 @@ import  FilterForm from '@/components/FilterForm/FilterForm';
 import styles from './index.module.scss';
 
 
-export default function Home() {
+export default function Home({ attributes }) {
 
   return (
     <>
     <div className={styles.body}>
-      <FilterForm />
+      <FilterForm data={attributes.tabs_data} />
     </div>
     </>
   )
 }
+
+export async function getStaticProps() {
+
+  return {
+    props: {
+      attributes: {
+        tabs_data: [
+          { label: 'ЖК', items: ['Академический', 'Басманный', 'Замоскворечье', 'Измайлово', 'Парус', 'Алые паруса','Дом на набережной', 'Воробьевы горы'] },
+          { label: 'Округ', items: ['Центральный', 'Зеленоградский', 'Восточный', 'Западный', 'Южный', 'Северный', 'Северо-Западный', 'Юго-Восточный'] },
+          { label: 'Район', items: ['Арбат', 'Аэропорт', 'Бабушкинский', 'Внуково', 'Гагаринский', 'Коньково', 'Чертаново', 'Люберцы'] },
+          { label: 'Метро', items: ['Третьяковская', 'Чертаново', 'Академическая', 'Шуваловская', 'Теплый стан', 'Ясенево', 'Измайлово'] },
+        ]
+      }
+    }
+  }
+}
+
 
